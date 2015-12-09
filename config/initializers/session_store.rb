21ec8016744a87097ca271a32410019d0bc3a8dd
Iyfe Ukachukwu <icukachuk@gmail.com>
2015-12-09 09:50:19 +0100

    Heroku

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+<<<<<<< HEAD
+Rails.application.config.session_store :cookie_store, key: '_myapp_session'
+=======
+Rails.application.config.session_store :cookie_store, key: '_hello_world_session'
+>>>>>>> a8f0495a4bbad1bb3827f8fc1f9e96e636cee9fd
